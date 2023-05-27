5242fe0d88cf252864491a70d3f6b9a3246dfc79
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-05-27 09:18:17 +0200

    Add console.js to test JS basics

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Complete all-in-one Javascript tutorial by Dave Gray.
 Link to the [YouTube Video](https://youtu.be/EfAl9bwzVZk).
 
 ### Main Topics
+- basic datatypes
+	- string
+	- number
+	- boolean
+	- undefined
+- variables
+- operators
+	- equality vs strict-equality
---
+++ console.js
@@ -0,0 +1,22 @@
+// a simple comment
+console.log("Hello, World!")
+
+// datatypes
+console.log(typeof "John");		// string
+console.log(typeof 42);			// number
+console.log(typeof true);		// boolean
+console.log(typeof myVar);		// undefined (!)
+
+// variables
+let myNumber = 0;
+let myString = "chars"
+let myFakeNumber = "0"
+
+let myCombo = myNumber + myString;
+
+console.log(typeof myCombo);
+
+console.log(myNumber == myFakeNumber);		// equality
+console.log(myNumber === myFakeNumber);		// strict equality
+
+
